f21188c92b430534b735481280bf38d96d3d8d0b
AbhiiAB <abhinav.chauhan@almabetter.com>
2023-06-26 23:54:42 +0530

    Initial

---
+++ testScript.js
@@ -0,0 +1,30 @@
+const firebaseConfig = {
+  apiKey: 'YOUR_API_KEY',
+  authDomain: 'YOUR_AUTH_DOMAIN',
+  projectId: 'YOUR_PROJECT_ID',
+  storageBucket: 'YOUR_STORAGE_BUCKET',
+  appId: 'YOUR_APP_ID',
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const storageRef = firebase.storage().ref();
+
+// Function to handle form submission
+function handleSubmit(event) {
+  event.preventDefault();
+
+  // Get the entered link
+  const linkInput = document.getElementById('linkInput');
+  const link = linkInput.value;
+
+  // Store the link in Firebase
+  const fileRef = storageRef.child('links/link.txt'); // Specify the desired file path
+  fileRef.putString(link)
+    .then(() => {
+      console.log('Link stored successfully!');
+    })
+    .catch((error) => {
+      console.error('Error storing link:', error);
+    });
+}
